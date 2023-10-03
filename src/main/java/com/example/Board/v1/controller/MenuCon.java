c4a0d71bfea370ddc5f828e2ff00b17ee23a7519
PARKBEB <it.parkbeb@gmail.com>
2023-10-03 10:49:16 +0900

    Service

--- src/main/java/com/example/Board/v1/controller/MenuCon.java
+++ src/main/java/com/example/Board/v1/controller/MenuCon.java
@@ -1,5 +1,6 @@
 package com.example.Board.v1.controller;
 
+import com.example.Board.v1.service.MenuSvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,25 +15,8 @@ public class MenuCon {
     @RequestMapping("/v1/menu")
     public String doMenu(Model model) { // Model객체는 컨트롤러 메서드에서 뷰로 데이터를 전달하는 데 사용
         // Data 만들기, List, Map
-        List<Map<String, Object>> list = new ArrayList<>();
+        List<Map<String, Object>> list = new MenuSvc().doList();
 
-        Map<String, Object> map = new HashMap<>();
-        map.put("No", "1");
-        map.put("name", "카페라떼");
-        map.put("kind", "커피");
-        map.put("price", "5,000");
-        map.put("reg_day", "2020.10.29");
-        map.put("mod_day", "2021.10.29");
-        list.add(map);
-
-        map.clear();
-        map.put("No", "1");
-        map.put("name", "아이스아메리카노");
-        map.put("kind", "커피");
-        map.put("price", "5,000");
-        map.put("reg_day", "2020.10.29");
-        map.put("mod_day", "2021.10.29");
-        list.add(map);
 
         // Data 송부
         model.addAttribute("list", list);
